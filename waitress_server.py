53d1bf93fee5bd1ba583afb1321e87ae783c643f
yudeep <endlessveins@gmail.com>
2023-04-07 13:07:31 -0400

    removed error logging

--- waitress_server.py
+++ waitress_server.py
@@ -1,4 +1,7 @@
+import logging
+
 from waitress import serve
 import Executor
 print('Server started at port 8080')
+Executor.app.logger.setLevel(logging.DEBUG)
 serve(Executor.app, host='0.0.0.0', port=8080)
